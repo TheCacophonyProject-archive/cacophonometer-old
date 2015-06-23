eed9b2e9de946acfb6ebe624dd7509521bde6558
CameronRP <cameron.ryan.pears@gmail.com>
2015-06-23 20:12:44 +1200

    Changed the New Rule activity to a more usable UI for small screens

--- app/src/main/java/com/thecacophonytrust/cacophonometer/activity/NewRuleActivity.java
+++ app/src/main/java/com/thecacophonytrust/cacophonometer/activity/NewRuleActivity.java
@@ -1,10 +1,12 @@
 package com.thecacophonytrust.cacophonometer.activity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -13,14 +15,23 @@ import com.thecacophonytrust.cacophonometer.R;
 import com.thecacophonytrust.cacophonometer.rules.Rule;
 import com.thecacophonytrust.cacophonometer.rules.RulesArray;
 
+import java.util.Calendar;
+
 public class NewRuleActivity extends ActionBarActivity {
 
 	final static private String LOG_TAG = "NewRuleActivity.java";
+
+	private int hour = -1;
+	private int minute = -1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_rule);
+		Calendar c = Calendar.getInstance();
+		hour = c.get(Calendar.HOUR_OF_DAY);
+		minute = c.get(Calendar.MINUTE);
+		updateSetStartTimeText();
 	}
 
 	@Override
@@ -28,6 +39,13 @@ public class NewRuleActivity extends ActionBarActivity {
 		getMenuInflater().inflate(R.menu.menu_new_rule, menu);
 		return true;
 	}
+
+	private void updateSetStartTimeText(){
+		Button startTimeEdit = (Button) findViewById(R.id.new_rule_set_start_time);
+		String s = String.format("%s:%s", hour, minute);
+		startTimeEdit.setText(s);
+	}
+
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -53,7 +71,6 @@ public class NewRuleActivity extends ActionBarActivity {
 	public void saveRule(View view){
 		EditText length = (EditText) findViewById(R.id.length_input);
 		EditText name = (EditText) findViewById(R.id.edit_rule_name);
-		TimePicker startTime = (TimePicker) findViewById(R.id.timePicker1);
 
 		if (name.getText().toString().equals("")){
 			Toast.makeText(getApplicationContext(), "Enter name for the rule", Toast.LENGTH_SHORT).show();
@@ -62,8 +79,8 @@ public class NewRuleActivity extends ActionBarActivity {
 		} else {
 			Rule rule = new Rule();
 			rule.setName(name.getText().toString());
-			rule.setStartTimeHour(startTime.getCurrentHour());
-			rule.setStartTimeMinute(startTime.getCurrentMinute());
+			rule.setStartTimeHour(hour);
+			rule.setStartTimeMinute(minute);
 			rule.setDuration(Integer.parseInt(length.getText().toString()));
 			if (rule.isValid()){
 				RulesArray.addRule(rule);
@@ -72,4 +89,23 @@ public class NewRuleActivity extends ActionBarActivity {
 			}
 		}
 	}
+
+	/**
+	 * Opens up a Time picker to enter in time of start of recording.
+	 * @param view
+	 */
+	public void setTime(View view){
+		TimePickerDialog timePicker = new TimePickerDialog(this, timePickerListener, hour, minute, true);
+		timePicker.show();
+	}
+
+	private TimePickerDialog.OnTimeSetListener timePickerListener =
+			new TimePickerDialog.OnTimeSetListener() {
+				public void onTimeSet(TimePicker view, int selectedHour,
+									  int selectedMinute) {
+					hour = selectedHour;
+					minute = selectedMinute;
+					updateSetStartTimeText();
+				}
+			};
 }
