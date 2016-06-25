9bd878c559422e025cc881c12dcfc07fc431c598
CameronRP <cameron.ryan.pears@gmail.com>
2016-06-25 16:06:42 +1200

    Chenged recording time format, and added recording date.

--- app/src/main/java/com/thecacophonytrust/cacophonometer/audioRecording/AudioCaptureRunnable.java
+++ app/src/main/java/com/thecacophonytrust/cacophonometer/audioRecording/AudioCaptureRunnable.java
@@ -28,7 +28,8 @@ public class AudioCaptureRunnable implements Runnable{
 
     private static final String LOG_TAG = "AudioCaptureRunnab.java";
 
-    private static DateFormat iso8601 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.UK);
+    private static DateFormat dateFormatDate = new SimpleDateFormat("yyyy-MM-dd", Locale.UK);
+    private static DateFormat dateFormatTime = new SimpleDateFormat("HH:mm:ss", Locale.UK);
     private static DateFormat fileFormat = new SimpleDateFormat("yyyyMMdd HHmmss", Locale.UK);
     private AudioRules.DataObject rule = null;
     private MediaRecorder mRecorder = null;
@@ -64,7 +65,8 @@ public class AudioCaptureRunnable implements Runnable{
 
             audioFile.put("localFilePath", filePath);
             audioFile.put("duration", rule.getDuration());
-            audioFile.put("startTimestamp", iso8601.format(date));
+            audioFile.put("startTimestamp", dateFormatTime.format(date));
+            audioFile.put("recordingDate", dateFormatDate.format(date));
             audioRecording.put("audioFileKey", AudioFile.addAndSave(audioFile));
             audioRecording.put("locationKey", Location.getMostRecentKey());
             audioRecording.put("hardwareKey", Hardware.getCurrentKey());
